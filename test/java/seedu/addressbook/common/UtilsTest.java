e3c9f4b16f8dd829955faf66a99c650025efdfec
He Xing Darius <Hexane.gmf@gmail.com>
2017-03-06 22:54:19 +0800

    Add more test cases

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -1,10 +1,9 @@
-package seedu.addressbook.common;
+package seedu.addressbook.common;  //this is where class Utils.java is found
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import org.junit.Test;
 
@@ -18,11 +17,13 @@ public class UtilsTest {
         assertFalse(Utils.isAnyNull(new Object(), new Object()));
         assertFalse(Utils.isAnyNull("test"));
         assertFalse(Utils.isAnyNull(""));
+        assertFalse(Utils.isAnyNull(new ArrayList<String>()));
 
         // non empty list with just one null at the beginning
         assertTrue(Utils.isAnyNull((Object) null));
         assertTrue(Utils.isAnyNull(null, "", new Object()));
         assertTrue(Utils.isAnyNull(null, new Object(), new Object()));
+        assertTrue(Utils.isAnyNull(new ArrayList<Object>(), new Object(), null));
 
         // non empty list with nulls in the middle
         assertTrue(Utils.isAnyNull(new Object(), null, null, "test"));
@@ -51,6 +52,7 @@ public class UtilsTest {
         // all objects unique
         assertAreUnique("abc", "ab", "a");
         assertAreUnique(1, 2);
+        assertAreUnique(new int[3], new int[5], new String());
 
         // some identical objects
         assertNotUnique("abc", "abc");
@@ -60,10 +62,12 @@ public class UtilsTest {
         assertNotUnique(null, 1, new Integer(1));
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
+        assertNotUnique("JesusChrist", "Lord", "Ala", "Buddha", "Ala");
     }
 
     private void assertAreUnique(Object... objects) {
-        assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
+        assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));  
+        //objects is an array, i.e. Object[] objects = new Object[];
     }
 
     private void assertNotUnique(Object... objects) {
